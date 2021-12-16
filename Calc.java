b4682f6d4ade18a4374c988769f9a30d8dede370
judaaaron <judaponce14@unitec.edu>
2021-12-15 22:35:15 -0600

    operaciones desde input

--- Calc.java
+++ Calc.java
@@ -1,16 +1,84 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Calc {
+    static Scanner leer = new Scanner(System.in);
+    static String input = "./test.calc";
+
     public static void main(String[] args) throws Exception {
-        ANTLRFileStream str = new ANTLRFileStream(args[0]);
+        boolean control = true;
+        while (control == true) {
+            System.out.println("---Calculadora basica---");
+            System.out.println("1. Realizar operaciones aritmeticas");
+            System.out.println("2. Salir");
+            int op = leer.nextInt();
+
+            switch (op) {
+                case 1: {
+
+                    String cadena;
+                    System.out.println("Ingrese operaciones aritmeticas: ");
+                    cadena = leer.next();
+
+                    FileWriter archivo = null;
+                    PrintWriter pw = null;
+                    try {
+                        archivo = new FileWriter(input, false);
+                        pw = new PrintWriter(archivo);
+                        pw.write(cadena);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+
+                            if (null != archivo)
+                                archivo.close();
+                        } catch (Exception e2) {
+                            e2.printStackTrace();
+                        }
+                    }
+
+                    System.out.print("El resultado es de: ");
+                    aritmetica(args[0]);
+                    System.out.println();
+                    System.out.println("Mostrando parse tree...");
+                    try {
+                        String arbol = "cmd /c start cmd.exe /K \"grun Calc start " + input + " -gui";
+                        Runtime.getRuntime().exec(arbol);
+
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+                    break;
+                }
+
+                case 2: {
+
+                    control = false;
+                    break;
+
+                }
+
+            }
+
+        }
+
+    }
+
+    public static void aritmetica(String args) throws Exception {
+        ANTLRFileStream str = new ANTLRFileStream(args);
         CalcLexer lex = new CalcLexer(str);
         CommonTokenStream tok = new CommonTokenStream(lex);
 
         CalcParser parser = new CalcParser(tok);
         ParseTree tree = parser.start();
-
         System.out.println(new MyVisitor().visit(tree));
+
     }
 }
 
